243fda549dd8a9f69ca5aaa234414d9633e349d2
Pawel <paw.mastalerz@gmail.com>
2018-11-23 13:17:47 +0100

    JQuery, bootstrap.bundle.js. Navbar prototype.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, LandingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, LandingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
